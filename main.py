cd691fe40fe3fccaa1f09ebf15deb802fbb56780
KUWABARA0217 <75057525+KUWABARA0217@users.noreply.github.com>
2020-11-26 15:55:03 +0900

    桑原の追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from diaries.DiarySample import DiarySample
+from diaries.KuwabaraDiary import DiaryKuwabara
 
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),DiaryKuwabara() ]
 
 for d in diaries:
     print("---------------------------------")
